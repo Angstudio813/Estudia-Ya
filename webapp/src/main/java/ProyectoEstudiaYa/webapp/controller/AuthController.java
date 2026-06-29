fa4d90d7ecfd7e59c61c3b1b851729504af898c7
Maximo <maximastudillo9@gmail.com>
2026-06-29 18:48:30 -0500

    LOGIN IMPLEMENTADO A ANGULAR

--- webapp/src/main/java/ProyectoEstudiaYa/webapp/controller/AuthController.java
+++ webapp/src/main/java/ProyectoEstudiaYa/webapp/controller/AuthController.java
@@ -1,13 +1,17 @@
 package ProyectoEstudiaYa.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
+    @Value("${app.frontend.login-url:http://localhost:4200/login}")
+    private String frontendLoginUrl;
+
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "redirect:" + frontendLoginUrl;
     }
 }
